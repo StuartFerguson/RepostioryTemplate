62a06f7017d5966cb725c0ff409d5e5f18ce9209
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-08-11 20:39:11 +0100

    Added db migration

---
+++ EstateReporting.Database/Migrations/20200811193716_transactionfeeupdates.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EstateReporting.Database.Migrations
+{
+    public partial class transactionfeeupdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FeeType",
+                table: "contractproducttransactionfee",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "contractproducttransactionfee",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FeeType",
+                table: "contractproducttransactionfee");
+
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "contractproducttransactionfee");
+        }
+    }
+}
