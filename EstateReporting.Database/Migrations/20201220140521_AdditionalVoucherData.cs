c3f2bf87575fee18e76df5296a6614f2e9ccad38
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-12-20 14:05:40 +0000

    Missed migration

---
+++ EstateReporting.Database/Migrations/20201220140521_AdditionalVoucherData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EstateReporting.Database.Migrations
+{
+    public partial class AdditionalVoucherData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "GenerateDateTime",
+                table: "voucher",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "IssuedDateTime",
+                table: "voucher",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TransactionId",
+                table: "voucher",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GenerateDateTime",
+                table: "voucher");
+
+            migrationBuilder.DropColumn(
+                name: "IssuedDateTime",
+                table: "voucher");
+
+            migrationBuilder.DropColumn(
+                name: "TransactionId",
+                table: "voucher");
+        }
+    }
+}
