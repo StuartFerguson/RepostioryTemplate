edf0d501b6f65ba3f65dad4fea6cbbed0adb3b14
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2021-04-15 17:12:48 +0100

    Swagger review

--- EstateReporting/Controllers/ReportingController.cs
+++ EstateReporting/Controllers/ReportingController.cs
@@ -8,11 +8,14 @@ namespace EstateReporting.Controllers
     using System.Diagnostics.CodeAnalysis;
     using System.Threading;
     using BusinessLogic;
+    using Common.Examples;
     using DataTransferObjects;
     using Factories;
     using Microsoft.AspNetCore.Mvc;
     using Models;
     using Repository;
+    using Swashbuckle.AspNetCore.Annotations;
+    using Swashbuckle.AspNetCore.Filters;
     using SortDirection = DataTransferObjects.SortDirection;
     using SortField = DataTransferObjects.SortField;
 
@@ -36,7 +39,7 @@ namespace EstateReporting.Controllers
         private readonly IModelFactory ModelFactory;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ReportingController"/> class.
+        /// Initializes a new instance of the <see cref="ReportingController" /> class.
         /// </summary>
         /// <param name="reportingManager">The reporting manager.</param>
         /// <param name="modelFactory">The model factory.</param>
@@ -70,6 +73,8 @@ namespace EstateReporting.Controllers
         /// <returns></returns>
         [HttpGet]
         [Route("estates/{estateId}/transactions/bydate")]
+        [SwaggerResponse(200, "OK", typeof(TransactionsByDayResponse))]
+        [SwaggerResponseExample(200, typeof(TransactionsByDayResponseExample))]
         public async Task<IActionResult> GetTransactionForEstateByDate([FromRoute] Guid estateId, [FromQuery(Name = "start_date")] String startDate, [FromQuery(Name = "end_date")] String endDate, CancellationToken cancellationToken)
         {
             // Get the data grouped as requested
@@ -92,6 +97,8 @@ namespace EstateReporting.Controllers
         /// <returns></returns>
         [HttpGet]
         [Route("estates/{estateId}/merchants/{merchantId}/transactions/bydate")]
+        [SwaggerResponse(200, "OK", typeof(TransactionsByDayResponse))]
+        [SwaggerResponseExample(200, typeof(TransactionsByDayResponseExample))]
         public async Task<IActionResult> GetTransactionForMerchantByDate([FromRoute] Guid estateId, [FromRoute] Guid merchantId, [FromQuery(Name = "start_date")] String startDate, [FromQuery(Name = "end_date")] String endDate, CancellationToken cancellationToken)
         {
             // Get the data grouped as requested
@@ -103,8 +110,18 @@ namespace EstateReporting.Controllers
             return this.Ok(response);
         }
 
+        /// <summary>
+        /// Gets the transaction for estate by week.
+        /// </summary>
+        /// <param name="estateId">The estate identifier.</param>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("estates/{estateId}/transactions/byweek")]
+        [SwaggerResponse(200, "OK", typeof(TransactionsByWeekResponse))]
+        [SwaggerResponseExample(200, typeof(TransactionsByWeekResponseExample))]
         public async Task<IActionResult> GetTransactionForEstateByWeek([FromRoute] Guid estateId, [FromQuery(Name = "start_date")] String startDate, [FromQuery(Name = "end_date")] String endDate, CancellationToken cancellationToken)
         {
             // Get the data grouped as requested
@@ -116,8 +133,19 @@ namespace EstateReporting.Controllers
             return this.Ok(response);
         }
 
+        /// <summary>
+        /// Gets the transaction for merchant by week.
+        /// </summary>
+        /// <param name="estateId">The estate identifier.</param>
+        /// <param name="merchantId">The merchant identifier.</param>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("estates/{estateId}/merchants/{merchantId}/transactions/byweek")]
+        [SwaggerResponse(200, "OK", typeof(TransactionsByWeekResponse))]
+        [SwaggerResponseExample(200, typeof(TransactionsByWeekResponseExample))]
         public async Task<IActionResult> GetTransactionForMerchantByWeek([FromRoute] Guid estateId, [FromRoute] Guid merchantId, [FromQuery(Name = "start_date")] String startDate, [FromQuery(Name = "end_date")] String endDate, CancellationToken cancellationToken)
         {
             // Get the data grouped as requested
@@ -129,8 +157,18 @@ namespace EstateReporting.Controllers
             return this.Ok(response);
         }
 
+        /// <summary>
+        /// Gets the transaction for estate by month.
+        /// </summary>
+        /// <param name="estateId">The estate identifier.</param>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("estates/{estateId}/transactions/bymonth")]
+        [SwaggerResponse(200, "OK", typeof(TransactionsByMonthResponse))]
+        [SwaggerResponseExample(200, typeof(TransactionsByMonthResponseExample))]
         public async Task<IActionResult> GetTransactionForEstateByMonth([FromRoute] Guid estateId, [FromQuery(Name = "start_date")] String startDate, [FromQuery(Name = "end_date")] String endDate, CancellationToken cancellationToken)
         {
             // Get the data grouped as requested
@@ -142,8 +180,19 @@ namespace EstateReporting.Controllers
             return this.Ok(response);
         }
 
+        /// <summary>
+        /// Gets the transaction for merchant by month.
+        /// </summary>
+        /// <param name="estateId">The estate identifier.</param>
+        /// <param name="merchantId">The merchant identifier.</param>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("estates/{estateId}/merchants/{merchantId}/transactions/bymonth")]
+        [SwaggerResponse(200, "OK", typeof(TransactionsByMonthResponse))]
+        [SwaggerResponseExample(200, typeof(TransactionsByMonthResponseExample))]
         public async Task<IActionResult> GetTransactionForMerchantByMonth([FromRoute] Guid estateId, [FromRoute] Guid merchantId, [FromQuery(Name = "start_date")] String startDate, [FromQuery(Name = "end_date")] String endDate, CancellationToken cancellationToken)
         {
             // Get the data grouped as requested
@@ -155,8 +204,21 @@ namespace EstateReporting.Controllers
             return this.Ok(response);
         }
 
+        /// <summary>
+        /// Gets the transaction for estate by merchant.
+        /// </summary>
+        /// <param name="estateId">The estate identifier.</param>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <param name="recordCount">The record count.</param>
+        /// <param name="sortDirection">The sort direction.</param>
+        /// <param name="sortField">The sort field.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("estates/{estateId}/transactions/bymerchant")]
+        [SwaggerResponse(200, "OK", typeof(TransactionsByMerchantResponse))]
+        [SwaggerResponseExample(200, typeof(TransactionsByMerchantResponseExample))]
         public async Task<IActionResult> GetTransactionForEstateByMerchant([FromRoute] Guid estateId, [FromQuery(Name = "start_date")] String startDate, [FromQuery(Name = "end_date")] String endDate, [FromQuery(Name = "record_count")] Int32 recordCount, [FromQuery(Name = "sort_direction")] SortDirection sortDirection, [FromQuery(Name = "sort_field")] SortField sortField, CancellationToken cancellationToken)
         {
             BusinessLogic.SortDirection sortDir = this.ModelFactory.ConvertFrom(sortDirection);
@@ -171,8 +233,21 @@ namespace EstateReporting.Controllers
             return this.Ok(response);
         }
 
+        /// <summary>
+        /// Gets the transaction for estate by operator.
+        /// </summary>
+        /// <param name="estateId">The estate identifier.</param>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <param name="recordCount">The record count.</param>
+        /// <param name="sortDirection">The sort direction.</param>
+        /// <param name="sortField">The sort field.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("estates/{estateId}/transactions/byoperator")]
+        [SwaggerResponse(200, "OK", typeof(TransactionsByOperatorResponse))]
+        [SwaggerResponseExample(200, typeof(TransactionsByOperatorResponseExample))]
         public async Task<IActionResult> GetTransactionForEstateByOperator([FromRoute] Guid estateId, [FromQuery(Name = "start_date")] String startDate, [FromQuery(Name = "end_date")] String endDate, [FromQuery(Name = "record_count")] Int32 recordCount, [FromQuery(Name = "sort_direction")] SortDirection sortDirection, [FromQuery(Name = "sort_field")] SortField sortField, CancellationToken cancellationToken)
         {
             BusinessLogic.SortDirection sortDir = this.ModelFactory.ConvertFrom(sortDirection);
